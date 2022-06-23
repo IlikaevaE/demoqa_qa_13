3a5b3281ca587e3cdd1c91121bae6cbbb6afeb9a
Elena Ilikaeva <gud_135win@mail.ru>
2022-06-23 11:13:12 +0300

    attachments added

--- src/test/java/tests/RegistrationFormWithPageObjectsTests.java
+++ src/test/java/tests/RegistrationFormWithPageObjectsTests.java
@@ -1,37 +1,48 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 import static tests.TestData.*;
 
 public class RegistrationFormWithPageObjectsTests extends TestBase {
     @Test
+    @DisplayName("Заполняем форму регистрации")
     void successfulTest() {
-        registrationFormPage.openPage()
-                .setFirstName(FIRST_NAME)
-                .setLastName(LAST_NAME)
-                .setUserEmail(EMAIL)
-                .setGender(GENDER)
-                .setUserNumber(PHONE_NUMBER)
-                .setDateOfBirth(DAY, MONTH, YEAR)
-                .setSubject(SUBJECT)
-                .setHobbies(HOBBY)
-                .addAttachment(ATTACHMENT)
-                .setAddress(ADDRESS)
-                .setState(STATE)
-                .setCity(CITY)
-                .clickSubmit(SUBMIT_VALUE);
+        step("Открываем форму регистрации", () -> {
+            registrationFormPage.openPage();
+        });
+        step("Заполняем необходимые поля", () -> {
+            registrationFormPage
+                    .setFirstName(FIRST_NAME)
+                    .setLastName(LAST_NAME)
+                    .setUserEmail(EMAIL)
+                    .setGender(GENDER)
+                    .setUserNumber(PHONE_NUMBER)
+                    .setDateOfBirth(DAY, MONTH, YEAR)
+                    .setSubject(SUBJECT)
+                    .setHobbies(HOBBY)
+                    .addAttachment(ATTACHMENT)
+                    .setAddress(ADDRESS)
+                    .setState(STATE)
+                    .setCity(CITY)
+                    .clickSubmit(SUBMIT_VALUE);
+        });
+        step("Проверяем результат", () -> {
+            registrationFormPage
+                    .checkResult("Student Name", FIRST_NAME + " " + LAST_NAME)
+                    .checkResult("Student Email", EMAIL)
+                    .checkResult("Gender", GENDER)
+                    .checkResult("Mobile", PHONE_NUMBER)
+                    .checkResult("Date of Birth", DAY + " " + MONTH + "," + YEAR)
+                    .checkResult("Subjects", SUBJECT)
+                    .checkResult("Hobbies", HOBBY)
+                    .checkResult("Picture", ATTACHMENT)
+                    .checkResult("Address", ADDRESS)
+                    .checkResult("State and City", STATE + " " + CITY);
+        });
 
-        registrationFormPage
-                .checkResult("Student Name", FIRST_NAME + " " + LAST_NAME)
-                .checkResult("Student Email", EMAIL)
-                .checkResult("Gender", GENDER)
-                .checkResult("Mobile", PHONE_NUMBER)
-                .checkResult("Date of Birth", DAY + " " +MONTH +"," + YEAR)
-                .checkResult("Subjects", SUBJECT)
-                .checkResult("Hobbies", HOBBY)
-                .checkResult("Picture", ATTACHMENT)
-                .checkResult("Address", ADDRESS)
-                .checkResult("State and City", STATE + " " + CITY);
 
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,15 +2,33 @@ package tests;
 
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attachments;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import pages.RegistrationFormPage;
+
+import static com.codeborne.selenide.Selenide.webdriver;
 
 public class TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1820x980";
         //  Configuration.holdBrowserOpen = true;
+    }
+    @AfterEach
+     void afterEach() {
+        Attachments.takeScreenshot("Last screenshot");
+        Attachments.browserConsoleLogs();
+        Attachments.pageSource();
+        Attachments.addVideo();
     }
 }
