e82f6e2cd44a21b07d0da55ee18e54c85f0a236a
IlikaevaE <gud_135win@mail.ru>
2022-06-29 14:51:29 +0300

    new project for telegram bot

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,21 +12,23 @@ import pages.RegistrationFormPage;
 
 public class TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+
     @BeforeAll
     static void beforeAll() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
-
+        Configuration.browser = "chrome";
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1820x980";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
+
     @AfterEach
-     void afterEach() {
+    void afterEach() {
         Attach.screenshotAs("Last screenshot");
         Attach.browserConsoleLogs();
         Attach.pageSource();
